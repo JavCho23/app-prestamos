9d9227ec536a50d3a23033b7ca0926223ec9ede7
NatalyPilar <50707479+NatalyPilar@users.noreply.github.com>
2019-12-12 09:22:00 -0500

    comentario

--- app/src/main/java/com/example/prestamos/detalle_prestamo.java
+++ app/src/main/java/com/example/prestamos/detalle_prestamo.java
@@ -2,6 +2,9 @@ package com.example.prestamos;
 
 import android.os.Bundle;
 
+import com.example.prestamos.Clases.ListaPrestamo;
+import com.example.prestamos.Clases.Prestamo;
+import com.example.prestamos.Clases.PrestamoAdaptador;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,8 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class detalle_prestamo extends AppCompatActivity {
+    private PrestamoAdaptador adaptador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,24 @@ public class detalle_prestamo extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+            TextView dni = findViewById(R.id.detalle_dni);
+            TextView nombre = findViewById(R.id.detalle_nombre);
+            TextView procedencia = findViewById(R.id.detalle_procedencia);
+            TextView telefono = findViewById(R.id.procedencia_telefono);
+            TextView correo = findViewById(R.id.detalle_correo);
+            TextView fecha = findViewById(R.id.fechaPrestamo);
+
+        final Prestamo prestamo = ListaPrestamo.getPrestamo(getIntent().getIntExtra("pos", 0));
+            dni.setText(prestamo.getDni());
+            nombre.setText(prestamo.getNombre());
+            procedencia.setText(prestamo.getProcedencia());
+            telefono.setText(prestamo.getTelefono());
+            correo.setText(prestamo.getCorreo());
+
+
+
     }
 
 }
